f3901f59499f64c4a37b09ce860f4f2a636f295a
kuroyamii <gerysastrawan123@gmail.com>
2022-01-23 22:37:43 +0800

    Update module

--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,22 @@
 package go_say_hello
 
+import (
+	"errors"
+	"fmt"
+)
+
 func SayHello() string {
 	return "Hello World"
+}
+
+func Addition(value interface{}) (int, error) {
+	switch num := value.(type) {
+	case int:
+		bil := value.(int)
+		return bil + 10, errors.New("No error")
+
+	default:
+		fmt.Println(num)
+		return 0, errors.New("Not an integer\n")
+	}
 }
